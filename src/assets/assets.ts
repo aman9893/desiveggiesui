6a7500ece3bbae3e361b79c90ebbf74baf5e021b
aman9893 <aman9893jain@gmail.com>
2026-05-28 19:03:22 +0530

    add

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,4 +1,4 @@
-import { TruckIcon, LeafIcon, ClockIcon, ShieldCheckIcon, MapPinIcon, PhoneIcon, MailIcon } from "lucide-react";
+import { TruckIcon, LeafIcon, MapPinIcon, PhoneIcon, MailIcon } from "lucide-react";
 import { SiFacebook, SiX, SiInstagram } from "@icons-pack/react-simple-icons";
 import hero_bg from "./hero_bg.jpeg";
 import delivery_truck from "./delivery_truck.svg";
