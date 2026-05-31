287300443fb2e4618d705dc1390c05368a18df4d
aman9893 <aman9893jain@gmail.com>
2026-05-31 16:02:59 +0530

    add

--- src/components/OrderTracking/LiveMap.tsx
+++ src/components/OrderTracking/LiveMap.tsx
@@ -65,9 +65,9 @@ export default function LiveMap({ order, liveLocation }: { order: any; liveLocat
         <>
             {order.status !== "Delivered" && order.status !== "Cancelled" && (
                 <div className="space-y-3">
-                    <div className="rounded-2xl overflow-hidden border border-app-border" style={{ height: 280 }}>
+                    <div className="rounded-2xl overflow-hidden border border-app-border relative z-40" style={{ height: 280 }}>
                         {liveLocation && liveLocation.lat !== 0 ? (
-                            <MapContainer center={[liveLocation.lat, liveLocation.lng]} zoom={15} style={{ height: "100%", width: "100%" }} zoomControl={false}>
+                            <MapContainer center={[liveLocation.lat, liveLocation.lng]} zoom={15} style={{ height: "100%", width: "100%", zIndex: 40 }} zoomControl={false}>
                                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                                 <Marker position={[liveLocation.lat, liveLocation.lng]} icon={truckIcon}>
                                     <Popup>
@@ -90,7 +90,7 @@ export default function LiveMap({ order, liveLocation }: { order: any; liveLocat
                                 <MapUpdater center={[liveLocation.lat, liveLocation.lng]} />
                             </MapContainer>
                         ) : order.shippingAddress?.lat && order.shippingAddress?.lng ? (
-                            <MapContainer center={[order.shippingAddress.lat, order.shippingAddress.lng]} zoom={15} style={{ height: "100%", width: "100%" }} zoomControl={false}>
+                            <MapContainer center={[order.shippingAddress.lat, order.shippingAddress.lng]} zoom={15} style={{ height: "100%", width: "100%", zIndex: 40 }} zoomControl={false}>
                                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                                 <Marker position={[order.shippingAddress.lat, order.shippingAddress.lng]} icon={destinationIcon}>
                                     <Popup>
